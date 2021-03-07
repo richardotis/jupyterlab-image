83bc3879d15f2d65c4bad088a3b227ee9289cbdc
Richard Otis <richard.otis@outlook.com>
2021-03-06 17:45:40 -0800

    Regex for filtering out root environment

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,4 +1,4 @@
 c.CondaKernelSpecManager.conda_only = True
 c.CondaKernelSpecManager.name_format = '{language} [env: {environment}]'
-c.CondaKernelSpecManager.env_filter = 'conda-root-py' 
+c.CondaKernelSpecManager.env_filter = r'^/opt/conda$' 
 c.KernelSpecManager.ensure_native_kernel = False
