d00da9d4e94ff6fead68eda85108b8d1b9c03e36
Richard Otis <richard.otis@outlook.com>
2021-03-06 17:17:42 -0800

    Filter out root conda environment

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,4 +1,4 @@
 c.CondaKernelSpecManager.conda_only = True
 c.CondaKernelSpecManager.name_format = '{language} [env: {environment}]'
-c.CondaKernelSpecManager.env_filter = 'conda-env-*' 
+c.CondaKernelSpecManager.env_filter = 'conda-root-py' 
 c.KernelSpecManager.ensure_native_kernel = False
