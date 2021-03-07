17bd0fa1b4953ec3148192b4a00ec084a157162b
Richard Otis <richard.otis@outlook.com>
2021-03-06 16:55:52 -0800

    Only include conda environments, not the base env

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,3 +1,4 @@
 c.CondaKernelSpecManager.conda_only = True
 c.CondaKernelSpecManager.name_format = '{language} [env: {environment}]'
+c.CondaKernelSpecManager.env_filter = 'conda-env-*' 
 c.KernelSpecManager.ensure_native_kernel = False
